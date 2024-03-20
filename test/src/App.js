fa8cc47ddfd676bae86070b4c611d5c119776324
Raguraj Sivanantham <99555816+sragu2000@users.noreply.github.com>
2024-03-20 16:27:13 +0000

    React CSS Module

---
+++ test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/landing/Landing';
+import About from './components/aboutus/About';
+
+function App() {
+  return (
+    <div>
+      <Landing />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ test/src/components/aboutus/About.js
@@ -0,0 +1,12 @@
+import styles from "./about.module.css"
+// import "./AboutNormal.css"
+const About = () => {
+    return (
+        <div className={styles.container}>
+        {/* <div className="container"> */}
+            Hello World !
+        </div>
+    )
+}
+
+export default About;
